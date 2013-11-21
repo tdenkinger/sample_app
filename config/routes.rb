2477e7282afa485e4da16e914bc25f7a608dd963
Troy Denkinger <troy@rgenerator.com>
2013-11-21 19:02:36 +0000

    Finished Chapter 3.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  let(:title){ "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{title} | Home")
     end
   end
   
@@ -24,7 +25,7 @@ describe "Static pages" do
  
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{title} | Help")
     end    
   end
   
@@ -37,7 +38,20 @@ describe "Static pages" do
  
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{title} | About Us")
     end   
   end
+  
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('contact')
+    end
+ 
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{title} | Contact")
+    end   
+  end  
 end
