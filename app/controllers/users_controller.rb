ba6cbdb58928477526bf1cd19c68d90bccfc06d4
Troy Denkinger <troy@rgenerator.com>
2014-04-17 21:48:53 +0000

    Adding feeds.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])    
   end
   
   def new
@@ -50,13 +51,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end 
-  
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end   
   end 
 
   def correct_user
