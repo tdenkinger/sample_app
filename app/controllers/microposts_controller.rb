355f3e6dfefc75355e383cfd42073cbc57088d18
Troy Denkinger <troy@rgenerator.com>
2014-04-23 17:50:16 +0000

    Microposts working completely.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
-
+  before_action :correct_user,   only: :destroy
+  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -13,11 +14,18 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url    
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+  
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end  
 end
